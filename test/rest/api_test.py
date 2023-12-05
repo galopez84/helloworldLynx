76aa9fa25b6f94fd1ac8b7d6974897215551c12a
Gonzalo Alonso <galopez84@gmail.com>
2023-12-05 20:09:53 +0100

    change api.py - api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -24,7 +24,27 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "3", "ERROR ADD"
         )
+        
+    def test_api_multiply(self):
+       url = f"{BASE_URL}/calc/multiply/3/2"
+       response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+       self.assertEqual(
+           response.status, http.client.OK, f"Error en la petición API a {url}"
+       )
+       self.assertEqual(
+           response.read().decode(), "6", "ERROR MULT"
+       )
 
+    def test_api_divide(self):
+       url = f"{BASE_URL}/calc/divide/10/2"
+       response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+       self.assertEqual(
+           response.status, http.client.OK, f"Error en la petición API a {url}"
+       )
+       self.assertEqual(
+           response.read().decode(), "5", "ERROR MULT"
+       )
+       
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
